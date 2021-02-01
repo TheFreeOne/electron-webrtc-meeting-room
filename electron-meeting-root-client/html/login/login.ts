313a06186fda0960cb370cb595bbedb762f3afa5
TheFreeOne <1357441309@qq.com>
2021-02-01 11:21:06 +0800

    登陆

---
+++ electron-meeting-root-client/html/login/login.ts
@@ -0,0 +1,24 @@
+
+import { ipcRenderer } from 'electron';
+import $ = require("jquery");
+import ChannelConstant from '../../util/ChannelConstant';
+
+let config = require('../../config.json');
+
+layui.use(['form'],()=>{
+    let form = layui.form;
+    form.on('submit(login-submit)',(formData)=>{
+        console.log(formData);
+        $.post(config.javaLoginServer+'/login.json',formData.field,(result)=>{
+            if(result.success){
+                ipcRenderer.send(ChannelConstant.LOGIN_SUCCESS,result.data);
+            }else{
+                layui.layer.msg(result.msg);
+            }
+        });
+        
+
+    });
+});
+
+export = {}
--- electron-meeting-root-client/main.ts
+++ electron-meeting-root-client/main.ts
@@ -1,49 +1,61 @@
-import { app, BrowserWindow, screen } from 'electron';
+import {app, BrowserWindow, globalShortcut, screen} from 'electron';
+import IpcMainListener from './util/IpcMainListener';
 
-app. allowRendererProcessReuse = false;
 let mainWindow: BrowserWindow;
+let ipcMainListener:IpcMainListener;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
-        title: '主窗口',
+        title: '会议室',
         width: 1366,
         height: 768,
-        
         minWidth: 800,
         minHeight: 600,
         maxWidth: screen.getPrimaryDisplay().workAreaSize.width,
         maxHeight: screen.getPrimaryDisplay().workAreaSize.height,
         show: false,
-        useContentSize: true,  // 这两个属性，完全去除边框
-        // frame: false,         // 这两个属性，完全去除边框
+        useContentSize: true,
         webPreferences: {
             nodeIntegration: true,
-           
             enableRemoteModule: false
         }
     });
 
     mainWindow.loadFile('./index.html');
     mainWindow.once('ready-to-show', () => {
-        mainWindow.show();
-        mainWindow.webContents.openDevTools({mode:'right'});
 
+        // 注册调试快捷键
+        globalShortcut.register('CommandOrControl+Alt+P', function () {
+            BrowserWindow.getFocusedWindow().webContents.openDevTools();
+        });
 
+        // 新建登陆窗口
+        let loginWindow = new BrowserWindow({
+            title: '会议室--登陆',
+            width: 340,
+            height: 450,
+            parent:mainWindow,
+            show: false,
+            resizable: false,
+            autoHideMenuBar:true,
+            useContentSize: true,
+            webPreferences: {
+                nodeIntegration: true,
+                enableRemoteModule: false
+            }
+        });
 
- 
+        loginWindow.loadFile('./html/login/login.html');
+        loginWindow.on("ready-to-show",()=>{
+            loginWindow.show();
+        });
 
-
-
-
-
-
-
+        ipcMainListener = new IpcMainListener(mainWindow,loginWindow);
     });
 }
 
 app.on('ready', () => {
     createWindow();
-
 });
 
 app.on("window-all-closed", () => {
@@ -51,3 +63,7 @@ app.on("window-all-closed", () => {
         app.quit();
     }
 });
+
+app.on('will-quit', function () {
+    globalShortcut.unregisterAll();
+})
